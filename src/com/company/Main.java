6f354a406a70e44af226002147362517f28c9e06
Lsc <1500093634@qq.com>
2016-12-26 22:16:31 +0800

    commit1

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(0);
+        System.out.println(method());
+    }
+
+    private static String method(){
+        return "aaa";
+    }
+}
