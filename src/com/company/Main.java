696589d5d34b30117f5db29cc47c96aea2a3098b
sugls <1500093634@qq.com>
2016-12-27 00:10:40 +0800

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         System.out.println(0);
         System.out.println(method());
+        System.out.println(1);
     }
 
     private static String method(){
